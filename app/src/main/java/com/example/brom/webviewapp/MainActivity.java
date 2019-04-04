42537899a8b9c3bde64eef60dd0a232f34e558d6
a18jakme <a18jakme@student.his.se>
2019-04-04 15:06:21 +0200

    externalochinternalwebview

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
       WebView webView = new WebView(this);
       setContentView(webView);
-      webView.loadUrl("http://wwwlab.iit.his.se/a18jakme/");
+      //webView.loadUrl("http://wwwlab.iit.his.se/a18jakme/");
+        webView.loadUrl("file:///android_asset/about.html");
         // 1. Create a WebView element in the layout file content_main.xml
         // -- Commit and push to your github fork
         // 2. Give the WebView element created in step 1 ID "my_webview"
